0d27745784fa51ba21cc9bdac60d8ad0b32f35ba
Todd Sifleet <todd.sifleet@gmail.com>
2024-08-21 04:05:13 -0500

    Initial Commit

---
+++ SleepTimer/SleepTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTimerApp.swift
+//  SleepTimer
+//
+//  Created by TODD SIFLEET on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SleepTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
