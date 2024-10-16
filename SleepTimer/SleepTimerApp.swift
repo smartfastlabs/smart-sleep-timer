f01c2d1dbd5a12f865b59d31d78cc66b18397ce1
Todd Sifleet <todd.sifleet@gmail.com>
2024-10-15 20:38:19 -0500

    add welcome screen

--- SleepTimer/SleepTimerApp.swift
+++ SleepTimer/SleepTimerApp.swift
@@ -51,7 +51,7 @@ class NotificationDelegate: NSObject , UNUserNotificationCenterDelegate{
 struct MenuBarIcon: View {
     @Environment(\.colorScheme) var colorScheme
     @StateObject var sleepTimer: SleepTimer
-   
+    
     var body: some View {
         let bedTime = self.sleepTimer.getBedTime()
         let iconName = if (self.sleepTimer.willSleepIn(minutes: 30)){
@@ -72,9 +72,66 @@ struct MenuBarIcon: View {
     }
 }
 
+struct WelcomeView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.openURL) private var openURL
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Welcome to Smart Sleep Timer")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            
+            Text("Your ultimate tool for better sleep.")
+                .font(.title2)
+                .multilineTextAlignment(.center)
+            
+            Image(nsImage: NSImage(named: "StatusBarIcon")!)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.blue)
+            
+            Text("Smart Sleep Timer runs in the background. Access it anytime from the menu bar.")
+                .font(.body)
+                .multilineTextAlignment(.center)
+            
+            Text("Once you've found Smart Sleep Timer in the Menu Bar click the button below to continue.")
+                .font(.body)
+                .multilineTextAlignment(.center)
+            
+            HStack {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                    
+                } label: {
+                    Text("I FOUND IT!")
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.borderedProminent).tint(.green)
+                Button {
+                    guard let url = URL(string: "https://smartfast.com/sleep-timer/support") else { return }
+                    openURL(url)
+                    
+                } label: {
+                    Text("I CAN'T FIND IT!")
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.borderedProminent).tint(.red)
+            }
+        }
+        .padding()
+        .frame(width: 450, height: 400)
+    }
+}
+
+var welcomeWindow: NSWindow?
+
 @main
 struct SleepTimerApp: App {
     @StateObject var sleepTimer: SleepTimer
+    @AppStorage("showWelcome") private var showWelcome = true
+
     
     private var notificDelegate : NotificationDelegate = NotificationDelegate()
     private var config: ConfigService =  ConfigService()
@@ -85,8 +142,24 @@ struct SleepTimerApp: App {
         notificDelegate.sleepTimer = sleepTimer
         notificDelegate.addObservers()
         UNUserNotificationCenter.current().delegate = notificDelegate
+        if showWelcome {
+            print("Show Welcome")
+            
+            welcomeWindow = NSWindow()
+            welcomeWindow?.contentView = NSHostingView(rootView: WelcomeView())
+            welcomeWindow?.identifier = NSUserInterfaceItemIdentifier(rawValue: "welcome")
+            welcomeWindow?.styleMask = [.closable, .titled]
+            welcomeWindow?.isReleasedWhenClosed = true
+            welcomeWindow?.center()
+            welcomeWindow?.becomeFirstResponder()
+            welcomeWindow?.orderFrontRegardless()
+            showWelcome = false;
+        }
     }
     
+    
+        
+        
     var body: some Scene {
         MenuBarExtra(isInserted: .constant(true)) {
             TimerView(timer: self.sleepTimer)
